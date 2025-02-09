dbce13303ee4dd8a50207a101dbe7705c3bc2014
KO Myung-Hun <komh78@gmail.com>
2025-02-10 00:05:19 +0900

    my: update configure.cmd
        modified:   configure.cmd

--- configure.cmd
+++ configure.cmd
@@ -5,5 +5,11 @@ d=$(dirname "$0" | tr '\\' /)
 n=configure
 test -f "$d/$n." || { echo "\`$d/$n' not found !!!"; exit 1; }
 
-opts=""
+opts="
+    --prefix=/@unixroot/usr/local
+    --enable-shared
+    --enable-static
+"
+
 "$d/$n" $opts "$@" 2>&1 | tee "$n.log"
+exit ${PIPESTATUS[0]}
